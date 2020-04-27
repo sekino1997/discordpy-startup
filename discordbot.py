f707c85c30b849069fa8acdad8961b2114d4ed58
sekino1997 <mickey.kei.427@gmail.com>
2020-04-27 23:46:40 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -16,6 +16,15 @@ async def on_command_error(ctx, error):
 @bot.command()
 async def ping(ctx):
     await ctx.send('pongPong')
+    
+# こんにちはメッセージ
+@client.event
+async def on_message(message):
+    if message.content.startswith("こんにちは"):
+        if client.user != message.author:
+            msg = "こんにちは " + message.author.name + "さん！"
+            await client.send_message(message.channel, msg)
+
 
 
 bot.run(token)
