9025fe58b7a989800ea0ad169cc8b44758588772
sekino1997 <mickey.kei.427@gmail.com>
2020-04-29 00:39:34 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -15,6 +15,7 @@ async def on_command_error(ctx, error):
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
 
+oizhdsfviozdbfv
 
 @bot.command()
 async def ping(ctx):
